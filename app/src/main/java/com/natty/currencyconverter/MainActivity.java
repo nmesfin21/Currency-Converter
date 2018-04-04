736421aa7dbe5a6c703ecc9c22fda55d37908c82
Natty <nmesfin2@go.shoreline.edu>
2018-04-03 18:05:39 -0700

    added a couple of text fields and button, works good

--- app/src/main/java/com/natty/currencyconverter/MainActivity.java
+++ app/src/main/java/com/natty/currencyconverter/MainActivity.java
@@ -2,6 +2,8 @@ package com.natty.currencyconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void convertCurrency(View view){
+        EditText amountOFMoney = (EditText) findViewById(R.id.amoutText);
+        double amountOFMoneyDouble = Double.parseDouble(amountOFMoney.getText().toString());
+        
+        EditText rate = (EditText) findViewById((R.id.rateText));
+        double rateDouble = Double.parseDouble((rate.getText().toString()));
+        
+        double result = rateDouble * amountOFMoneyDouble;
+        String resString = Double.toString(result);
+        
+        EditText convertionResult = (EditText) (findViewById(R.id.resultText));
+        convertionResult.setText(resString);
+        
+        
+
     }
 }
